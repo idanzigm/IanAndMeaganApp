0507ac5ea3e41a6cb2ae5ae1e9f16c63e173663f
idanzigm <97111242+idanzigm@users.noreply.github.com>
2022-08-09 15:57:20 -0400

    Initial commit

---
+++ RegistryBackend/src/main/java/com/IanAndMeaganApp/RegistryBackend/RegistryBackendApplication.java
@@ -0,0 +1,14 @@
+package com.IanAndMeaganApp.RegistryBackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RegistryBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RegistryBackendApplication.class, args);
+	}
+
+	
+}
---
+++ RegistryBackend/src/test/java/com/IanAndMeaganApp/RegistryBackend/RegistryBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.IanAndMeaganApp.RegistryBackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RegistryBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
