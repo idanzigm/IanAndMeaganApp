b8617ad79f4fe15f19066dd07b1d741accc653ed
idanzigm <97111242+idanzigm@users.noreply.github.com>
2022-12-08 15:36:39 -0500

    updates to Cloudformatinn

--- RegistryBackend/src/main/java/com/IanAndMeaganApp/RegistryBackend/Controller.java
+++ RegistryBackend/src/main/java/com/IanAndMeaganApp/RegistryBackend/Controller.java
@@ -1,2 +1,14 @@
-package com.IanAndMeaganApp.RegistryBackend;public class Controller {
+package com.IanAndMeaganApp.RegistryBackend;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Controller {
+
+    @RequestMapping(method=RequestMethod.GET, value="/api/info")
+    public String getTest() {
+        return "Working";
+    }
 }
--- RegistryBackend/src/main/java/com/IanAndMeaganApp/RegistryBackend/RegistryBackendApplication.java
+++ RegistryBackend/src/main/java/com/IanAndMeaganApp/RegistryBackend/RegistryBackendApplication.java
@@ -2,6 +2,8 @@ package com.IanAndMeaganApp.RegistryBackend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 public class RegistryBackendApplication {
@@ -9,6 +11,5 @@ public class RegistryBackendApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(RegistryBackendApplication.class, args);
 	}
-
 	
 }
